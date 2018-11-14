57f6ce1941eca957a431671aa7f3b17e90ca61b4
Trang Nguyen <trangthi.nguyen@hubcba.com>
2018-11-14 17:28:51 +0700

    Student class

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -40,17 +40,74 @@ public class EmployeeTest {
 //		fib.fib(num);			
 		
 		/* Print star */
-		SortStars sort = new SortStars();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter number: ");
-		int num = scanner.nextInt();
-		String increate = scanner.next();
-		Boolean inc = new Boolean(increate);
-		sort.sort(num, inc);
+//		SortStars sort = new SortStars();
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Please enter number: ");
+//		int num = scanner.nextInt();
+//		String increate = scanner.next();
+//		Boolean inc = new Boolean(increate);
+//		sort.sort(num, inc);
 		
 		/* Students */
 		Student student = new Student();
 		
+		StudentList students = new StudentList();
+		students.addStudent(new Student(1, "Trang", "Nguyen", 28));
+		students.addStudent(new Student(2, "Minh", "Nguyen", 29));
+
+//		System.out.println("List of students:");
+//		System.out.println(students.getStudents());
+//		System.out.println(students.getSize());
+//		students.addStudent(new Student(3, "Hung", "Nguyen", 28));
+//		
+//		System.out.println(students.getStudents());
+//		System.out.println(students.getSize());
+//		
+//		System.out.println("Remove student");
+//		students.removeStudent(0);
+//		System.out.println(students.getStudents());
+//		System.out.println(students.getSize());
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please select option to make the feature below: ");
+		System.out.println("1. Add student");
+		System.out.println("2. Delete student");
+		System.out.println("3. List students");
+		int option;
+		do {
+			option = scanner.nextInt();
+			switch (option) {
+			case 1:
+				System.out.println("Please enter new Student");
+				System.out.println("Enter id:");
+				int id = scanner.nextInt();
+				System.out.println("Enter First name:");
+				String fName = scanner.next();
+				System.out.println("Enter Last name:");
+				String lName = scanner.next();
+				System.out.println("Enter age:");
+				int age = scanner.nextInt();
+				
+				students.addStudent(new Student(id, fName, lName, age));
+				System.out.println("Add Student successfully!. List students:");
+				students.pasteStudents(students.getStudents());
+				break;
+			case 2:
+				System.out.println("Please enter student who you want to deleted (by id):");
+				int idDel = scanner.nextInt();
+				students.removeStudent(idDel);
+				System.out.println("List students after deleted: ");
+				students.pasteStudents(students.getStudents());
+				break;
+			case 3:
+				System.out.println("List students: ");
+				students.pasteStudents(students.getStudents());
+				break;
+			default:
+				break;
+			}
+		} while (option != 4);
 		
 		
 	   }
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,4 @@
+import java.io.PushbackReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,38 @@ public class Student {
 	    this.lastName = lastName;
 	    this.age = age;
 	}
+	
+	public int getId() {
+	    return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String fname) {
+		this.firstName = fname;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lname) {
+		this.lastName = lname;
+	}
+	
+	public int getAge() {
+	    return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
 
 class StudentList {
@@ -35,6 +68,26 @@ class StudentList {
 	// get list student
 	public List<Student> getStudents() {
 		return students;
+	}
+	
+	public int getSize() {
+		return students.size();
+	}
+	
+	public Student getById(int id) {
+		return students.get(id);
+	}
+	
+	private String getDetail(Student student) {
+		return student.getId() + ". " + student.getFirstName() + " " + student.getLastName() + "   " + student.getAge();
+	}
+	
+	public void pasteStudents(List<Student> students) {
+		// TODO Auto-generated method stub
+		for (int i =0;i<students.size();i++) {
+			Student stu = getById(i);
+			System.out.println(getDetail(stu));
+		}
 	}
 }
 
